588fdd17f4f47e4aee526a64906640979bfe7f82
Erolfina <floregridaine@gmail.com>
2023-01-11 17:38:00 +1100

    save data when press add button

---
+++ RecipePlease/Controller/SearchViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  RecipePlease
+//
+//  Created by Flore Gridaine on 2023-01-09.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    //MARK: Properties
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var labelIngredients: UILabel!
+    var ingredientsInFridge: [String?] = [""]
+    var listIngrendient = ""
+    
+    //MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        labelIngredients.text = ""
+    }
+    
+    //MARK: Methods
+    
+    @IBAction func addIngredients(_ sender: Any) {
+        textField.clearButtonMode = .always
+        guard let ingredient = textField.text else { return }
+        textField.text = ""
+        ingredientsInFridge.append(ingredient)
+        labelIngredients.text! +=  ingredient+"\n"
+        
+    }
+    
+    private func updateIngredientsList() {
+       
+    }
+}
+
+extension SearchViewController: UITextFieldDelegate {
+    
+    //MARK: Methods Keyboard
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+        
+    }
+    
+    @IBAction func dismiss(_ sender: UITapGestureRecognizer) {
+         textField.resignFirstResponder()
+    }
+    
+    
+}
